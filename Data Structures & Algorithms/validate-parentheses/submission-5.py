0363335ebc72e269bbfcaec9eb41b23578bc39c9
Henrique Negri <henriquenegri84@gmail.com>
2026-07-22 16:22:45 -0300

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,32 @@
+# percorre a string
+# cria um contador de fechamento
+# para cada caractere da string, se for um de abertura ('(', '{', '[') soma no contador
+# se for de fechamento (')', '}', ']') , subtrai do contador
+# retorna contador == 0
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s: return True
+
+        count_open = deque()
+
+
+        for char in s:
+            ord_char = ord(char)
+            open_chars = [ord('('), ord( '{'), ord('[')]
+            closes_chars = [ord(')'), ord('}'), ord(']')]
+
+
+            if ord_char in open_chars:
+                count_open.append(char)
+            elif ord_char in closes_chars and len(count_open) > 0 :
+                last_open = count_open[-1]
+                if ord(last_open) == ord('(') and ord_char == ord(')') or ord(last_open) == ord('{') and ord_char == ord('}') or ord(last_open) == ord('[') and ord_char == ord(']'):
+                    count_open.pop()
+                else:
+                    return False
+            else:
+                return False
+
+
+        return len(count_open) == 0
